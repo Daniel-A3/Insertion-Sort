8514174658f2fd3159ca57675638b1c960b31df3
Daniel <66734096+Daniel-A3@users.noreply.github.com>
2022-03-06 19:45:58 +0000

    Add files via upload

---
+++ insertionSort.py
@@ -0,0 +1,20 @@
+#INSERSTION SORT ALGORITHM
+
+def insertionSort(arrayOne): 
+    count = 0
+
+    for i in range(1, len(arrayOne)):
+        valueSorted = False
+        count = i
+        while arrayOne[count] < arrayOne[count-1] and count != 0:
+            # Swamps the two values
+            temp = arrayOne[count]
+            arrayOne[count] = arrayOne[count-1]
+            arrayOne[count-1] = temp
+            count -= 1         
+
+    return arrayOne
+
+arrayToSort = [9,2,7,4,5,3]
+
+print("The sorted array is : ", insertionSort(arrayToSort))
